5fb95645867f160dd14f23cb6abc6fb092163d23
Josue Alban <josue.alban.contractor@bbva.com>
2022-03-25 23:33:35 -0500

    feat(*): adding files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+        <div v-for="(value, key) in comic" v-bind:key="key">
+          {{ key }}: {{ value }}
+        </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      comic: null,
+      comicId: Math.floor(Math.random() * 806 + 1).toString()
+    }
+  },
+  mounted () {
+    fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
+      .then(response => response.json())
+      .then(data =>  this.comic = data);
   }
 }
 </script>
