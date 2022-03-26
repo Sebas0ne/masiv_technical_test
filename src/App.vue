f5bd6f8b7fcb7b9b56e730e4092de34509c9aa90
Josue Alban <josue.alban.contractor@bbva.com>
2022-03-26 01:43:45 -0500

    feat(*): implementation of card with comic information and random comic button

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-        <div v-for="(value, key) in comic" v-bind:key="key">
-          {{ key }}: {{ value }}
+  <v-app>
+    <NavBar />
+    <v-main>
+      <v-card class="mx-auto" max-width="300">
+        <v-img :src="comic.img" height="250px"></v-img>
+
+        <v-card-title> {{ comic.title }} ( {{ comic.num }} ) </v-card-title>
+
+        <v-card-subtitle> Year: {{ comic.year }} </v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>Month: {{ comic.month }} / Day: {{ comic.day }}</div>
+        </v-card-text>
+
+        <div class="text-center" @click="showAlert">
+          <span bold>Califica el comic!</span>
+          <v-rating
+            v-model="rating"
+            color="blue"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+            large
+          ></v-rating>
         </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+        <v-card-actions>
+          <v-btn color="blue" text v-on:click="randomComic()">
+            Random Comic
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              {{ comic.transcript }}
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import NavBar from "./components/NavBar";
+import Swal from 'sweetalert2'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    NavBar,
   },
-  data () {
+  data() {
     return {
       comic: null,
-      comicId: Math.floor(Math.random() * 806 + 1).toString()
+      comicId: Math.floor(Math.random() * 2190 + 1).toString(),
+      show: false,
+      rating: 0
+    };
+  },
+  mounted() {
+    fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
+      .then((response) => response.json())
+      .then((data) => (this.comic = data));
+  },
+  methods: {
+    randomComic: function () {
+      fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
+        .then((response) => response.json())
+        .then((data) => (this.comic = data));
+      this.resetValues();
+    },
+    resetValues: function () {
+      this.comicId = Math.floor(Math.random() * 1000 + 1).toString();
+      this.rating = 0;
+    },
+    showAlert: function () {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'info',
+        title: `Comic Stars: ${ this.rating }`,
+        showConfirmButton: false,
+        timer: 1500
+      })
     }
   },
-  mounted () {
-    fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
-      .then(response => response.json())
-      .then(data =>  this.comic = data);
-  }
-}
+};
 </script>
 
 <style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+            <v-btn
+              target="_blank"
+              text
+            >
+              <span class="mr-2">Masiv Technical Test</span>
+            </v-btn>        
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Documentation</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
