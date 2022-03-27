aca0ada6b22c83ca6582d3bfcde422c3c68a1f3e
Josue Alban <josue.alban.contractor@bbva.com>
2022-03-27 07:04:05 -0500

    feat(*): optimizing code,  adding loader and adding alert for rate comic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <NavBar />
     <v-main>
-      <v-card class="mx-auto" max-width="300">
+      <v-card class="mx-auto" max-width="300" v-if="comic && loader === false">
         <v-img :src="comic.img" height="250px"></v-img>
 
         <v-card-title> {{ comic.title }} ( {{ comic.num }} ) </v-card-title>
@@ -14,7 +14,7 @@
         </v-card-text>
 
         <div class="text-center" @click="showAlert">
-          <span bold>Califica el comic!</span>
+          <span bold>Rate comic!</span>
           <v-rating
             v-model="rating"
             color="blue"
@@ -48,52 +48,69 @@
           </div>
         </v-expand-transition>
       </v-card>
+      <v-sheet class="pa-3" v-else>
+        <v-skeleton-loader
+          class="mx-auto"
+          max-width="300"
+          type="card"
+        ></v-skeleton-loader>
+      </v-sheet>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 export default {
   name: "App",
   components: {
     NavBar,
   },
+  inject: {
+    theme: {
+      default: { isDark: false },
+    },
+  },
   data() {
     return {
       comic: null,
       comicId: Math.floor(Math.random() * 2190 + 1).toString(),
       show: false,
-      rating: 0
+      rating: 0,
+      loader: false,
     };
   },
   mounted() {
-    fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
-      .then((response) => response.json())
-      .then((data) => (this.comic = data));
+    this._getComic();
   },
   methods: {
     randomComic: function () {
-      fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
+      this._resetValues();
+      this._getComic();
+    },
+    async _getComic() {
+      this.loader = true;
+      await fetch(`https://xkcd.com/${this.comicId}/info.0.json`)
         .then((response) => response.json())
         .then((data) => (this.comic = data));
-      this.resetValues();
+      this.loader = false;
     },
-    resetValues: function () {
+    _resetValues() {
       this.comicId = Math.floor(Math.random() * 1000 + 1).toString();
       this.rating = 0;
     },
     showAlert: function () {
       Swal.fire({
-        position: 'top-end',
-        icon: 'info',
-        title: `Comic Stars: ${ this.rating }`,
+        position: "top",
+        icon: "info",
+        title: `Comic Stars: ${this.rating}`,
         showConfirmButton: false,
-        timer: 1500
-      })
-    }
+        timer: 3500,
+        toast: true,
+      });
+    },
   },
 };
 </script>
